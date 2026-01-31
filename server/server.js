23dbed4a37618ac4d6fb92be23d5d9df460f08d4
amarmeshram567 <meshramamar567@gmail.com>
2026-01-31 11:13:03 +0530

    project

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const app = express()
 
 const allowedOrigins = [
-    'http://localhost:5173', 'https://ai-counsellor-htmf.vercel.app'
+    'http://localhost:5173',
 ]
 
 app.use(cors({
